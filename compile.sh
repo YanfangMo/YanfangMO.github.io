8ba63c0d8e80c82aead073902c72ce947ed288ad
Yanfang MO <ymoaa@connect.ust.hk>
2024-02-07 19:10:50 +0800

    update

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 # Please run the command `./compile.sh`
 # After update, please click the source control button to commit and upload the code.
 
-python2 jemdoc.py index awards publications service teaching
+python2 jemdoc.py index awards research publications service teaching resources
