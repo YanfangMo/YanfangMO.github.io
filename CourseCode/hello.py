eabe8986101cf4b2fd95fe3cd0c612c91cd49fbf
Yanfang MO <ymoaa@connect.ust.hk>
2024-08-21 18:42:59 +0800

    update

--- CourseCode/hello.py
+++ CourseCode/hello.py
@@ -1,4 +1,16 @@
-count = 0
-for k in range(1, 11, 2):
-    count=count+1
-    print(count)
+import array
+arr = array.array('i', [1, 2, 3, 1, 5])
+print("The new created array is : ", end="")
+for i in range(0, 5):
+    print(arr[i], end=" ")
+print("\r")
+print("The popped element is : ", end="")
+print(arr.pop(2))
+print("The array after popping is : ", end="")
+for i in range(0, 4):
+    print(arr[i], end=" ")
+print("\r")
+arr.remove(2)
+print("The array after removing is : ", end="")
+for i in range(0, 3):
+    print(arr[i], end=" ")
